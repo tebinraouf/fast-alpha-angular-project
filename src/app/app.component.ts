53e954be89e6ca71be231cd82e792d3be26c9612
Tebin Raouf <tebin.raouf@thomsonreuters.com>
2022-10-06 23:06:34 -0400

    add fast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FASTButton } from '@microsoft/fast-foundation';
+
+FASTButton;
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {​​​​​​ CUSTOM_ELEMENTS_SCHEMA }​​​​​​ from '@angular/core';
+
 
 @NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [
     AppComponent
   ],
